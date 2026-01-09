50e185a4383a9bf6c7f1ea04bf82629c5ab75462
Duskara <davidantillon905@gmail.com>
2026-01-08 23:36:08 -0600

    Added initial project structure.

---
+++ just.config.ts
@@ -0,0 +1,69 @@
+import { argv, parallel, series, task } from "just-scripts";
+import {
+  CopyTaskParameters,
+  cleanTask,
+  cleanCollateralTask,
+  copyTask,
+  coreLint,
+  mcaddonTask,
+  setupEnvironment,
+  ZipTaskParameters,
+  STANDARD_CLEAN_PATHS,
+  DEFAULT_CLEAN_DIRECTORIES,
+  getOrThrowFromProcess,
+  watchTask,
+} from "@minecraft/core-build-tasks";
+import fs from "fs";
+import path from "path";
+setupEnvironment(path.resolve(__dirname, ".env"));
+const projectName = getOrThrowFromProcess("PROJECT_NAME");
+const fsp = fs.promises;
+const copyTaskOptions: CopyTaskParameters = {
+  copyToBehaviorPacks: [`./behavior_packs/${projectName}`],
+  copyToScripts: ["./dist/scripts"],
+  copyToResourcePacks: [`./resource_packs/${projectName}`],
+};
+const mcaddonTaskOptions: ZipTaskParameters = {
+  ...copyTaskOptions,
+  outputFile: `./dist/packages/${projectName}.mcaddon`,
+};
+task("lint", coreLint(["scripts/**/*.{ts,js}"], argv().fix));
+task("build", async () => {
+  const src = path.join(__dirname, "scripts");
+  const dest = path.join(__dirname, "dist/scripts");
+
+  async function copyDir(from, to) {
+    await fsp.rm(to, { recursive: true, force: true });
+    await fsp.mkdir(to, { recursive: true });
+    const entries = await fsp.readdir(from, { withFileTypes: true });
+    for (const entry of entries) {
+      const srcPath = path.join(from, entry.name);
+      const destPath = path.join(to, entry.name);
+      if (entry.isDirectory()) {
+        await copyDir(srcPath, destPath);
+      } else {
+        await fsp.copyFile(srcPath, destPath);
+      }
+    }
+  }
+
+  await copyDir(src, dest);
+});
+task("clean-local", cleanTask(DEFAULT_CLEAN_DIRECTORIES));
+task("clean-collateral", cleanCollateralTask(STANDARD_CLEAN_PATHS));
+task("clean", parallel("clean-local", "clean-collateral"));
+task("copyArtifacts", copyTask(copyTaskOptions));
+task("package", series("clean-collateral", "copyArtifacts"));
+task(
+  "local-deploy",
+  watchTask(
+    [
+      "scripts/**/*.{ts,js}",
+      "behavior_packs/**/*.{json,lang,tga,ogg,png}",
+      "resource_packs/**/*.{json,lang,tga,ogg,png}",
+    ],
+    series("clean-local", "build", "package")
+  )
+);
+task("createMcaddonFile", mcaddonTask(mcaddonTaskOptions));
+task("mcaddon", series("clean-local", "build", "createMcaddonFile"));
